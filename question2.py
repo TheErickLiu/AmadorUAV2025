a63c1391f747ba123660d58ff803c8c8c3b93b07
TheErickLiu <coolcropper@gmail.com>
2024-09-29 17:43:02 -0700

    q2 done

--- question2.py
+++ question2.py
@@ -3,6 +3,8 @@ from typing import List, Tuple
 import numpy as np
 import math
 import json
+import matplotlib.pyplot as plt
+from shapely.plotting import plot_polygon
 
 class GeoFence:
     def __init__(self, coords: List[Tuple]):
@@ -16,26 +18,29 @@ class GeoFence:
             edgelist.append(LineString((self.coords[i],self.coords[i + 1])))
         return edgelist
 
-    def intersected_lines(self, line: LineString) -> List[LineString]:
-        """Return a list of geoFence edges that intersect with line"""
-        intersection_lines = []
-        intersections = line.intersection(self.polygon)
-        if intersections.geom_type == "LineString":
-            return intersection_lines
-        elif intersections.geom_type == "MultiLineString":
-            for seg in intersections.geoms:
-                intersection_lines.append(seg)
-        return intersection_lines
+    def find_polygon_point(self, line):
+        """Find the geofence point with edges intersected by a waypoint line"""
+        edges = []
+        for idx, edge in enumerate(self.edges()):
+            if line.intersects(edge):
+                edges.append((idx, edge))
+
+        pt_id, pt = -1, None
+        if len(edges) == 2:
+            pt_id, pt = edges[1][0], edges[1][1].coords[0]
+        
+        return pt_id, pt
 
     def get_point_mid_degree(self, i):
+        """Finds angle of bisecting angle between lines"""
         line1 = LineString((self.coords[i], self.coords[i-1]))
         line2 = LineString((self.coords[i], self.coords[i+1]))
         line_degrees = [d + 360 if d < 0 else d for d in [line_degree(line1), line_degree(line2)]]
         mid_angle = line_degrees[1] + (line_degrees[0] - line_degrees[1]) / 2
-        #print(line_degree(line1), line_degree(line2), line_degrees, mid_angle)
         return mid_angle
 
     def get_adjust_point(self, id, dist=25):
+        """Finds point 25 ft from a geofence point to adjust outside waypoint to and detour intersecting path"""
         coords = self.polygon.exterior.coords
         mid_angle = self.get_point_mid_degree(id)
         radius = dist / 364000
@@ -43,13 +48,7 @@ class GeoFence:
         if self.polygon.contains(Point(x, y)):
             return (x, y)
         return get_point_in_radius(coords[id], mid_angle+180, radius)
-    
-    def find_polygon_edge(self, line):
-        for i, edge in enumerate(self.edges()):
-            if line.intersects(edge):
-                return i, edge
-        return -1, None
-    
+
     def nearest_point_on_polygon(self, point):
         """Return the nearest exterior point on polygon"""
         nearest_pt, nearest_dist = None, np.inf
@@ -70,35 +69,6 @@ class Plan:
     
     def home_point(self, altitude=50):
         return self.wayPoints[0] + [altitude]
-    
-    def find_detour_point(self, line, betweenpoint, dist=25):
-        # Unpack coordinates
-        x1, y1 = line.coords[0]
-        x2, y2 = line.coords[1]
-        px, py = betweenpoint
-        
-        # Find vector perpendicular from line
-        slope = (y2 - y1) / (x2 - x1)
-        inv_slope = -1 / slope if slope != 0 else 0
-        print(f"slope = {slope}, inv_slope = {inv_slope}")
-
-        # Find direction based on the position of the point relative to the line
-        cross_product = (x2 - x1) * (py - y1) - (y2 - y1) * (px - x1)
-        #print(f"cross_product = {cross_product}")
-        
-        # Point is to the left of the line, adjust direction
-        direction = np.arctan(inv_slope)
-        direction += np.pi  # Adding 180 degrees in radians
-        #print(f"direction = {direction}")
-
-        # Convert distance in feet to latitude
-        dist = dist / 364000
-
-        # Calculate the detour point
-        detour_x = px + dist * np.cos(direction)
-        detour_y = py + dist * np.sin(direction)
-
-        return Point(detour_x, detour_y)
 
     def adjust_outside_waypoints(self):
         """Adjust waypoints that are outside of geoFence to inside"""
@@ -117,19 +87,12 @@ class Plan:
 
         detour_points = []
         for i in range(len(self.wayPoints) - 1):
-            line = LineString((self.wayPoints[i], self.wayPoints[i+1]))
-            interline = self.geoFence.intersected_lines(line)
-            if len(interline) > 0:
-                edges = []
-                for l in interline:
-                    edges.append(self.geoFence.find_polygon_edge(l))
-
-                target_point_id = edges[1][0]
-                detour_point = self.geoFence.get_adjust_point(target_point_id)
+            line = LineString(self.wayPoints[i:i+2])
+            pt_id, pt = self.geoFence.find_polygon_point(line)
+            if pt:
+                detour_point = self.geoFence.get_adjust_point(pt_id)
                 detour_points.append((i+1, detour_point))
-        
-        #print(detour_points)
-        
+
         for i, d in enumerate(detour_points):
             self.wayPoints.insert(d[0] + i, d[1])
 
@@ -193,14 +156,23 @@ class Plan:
                 "items": items,        
             }
         }
+    
+    def plot(self):
+        """Plots the plan & saves as png"""
+        fig = plt.figure(figsize=(9, 9))
+        ax = fig.add_subplot(111)
+        plot_polygon(self.geoFence.polygon, ax=ax, add_points=True, color="blue")
+        plot_polygon(Polygon(self.wayPoints), ax=ax, add_points=True, color="green")
+        plt.savefig("nav.png")
 
 def get_point_in_radius(point, angle, radius):
+    """Gets x and y coords of an edge"""
     x = point[0] + radius * math.cos(math.radians(angle))
     y = point[1] + radius * math.sin(math.radians(angle))
     return (x, y)
 
-# get degree of an edge
 def line_degree(line):
+    """Get degree of an edge"""
     angle = math.atan2(line.coords[1][1] - line.coords[0][1], line.coords[1][0] - line.coords[0][0])
     angle_degree = math.degrees(angle)
     return angle_degree
@@ -222,22 +194,15 @@ def read_input(in_file):
 
     return geofence_coords, waypoints
 
-def shared_point(line1, line2):
-        """Return the shared point of two lines"""
-        if line1.coords[0] == line2.coords[1]:
-            return line1.coords[0]
-        if line1.coords[1] == line2.coords[0]:
-            return line1.coords[1]
-        return None
-
 def main():
     input = read_input("navigate.txt")
     geofence = GeoFence(input[0])
     plan = Plan(geofence, input[1])
     plan.adjust_points()
     plandict = plan.create_plan_dict()
-    #print(plan.wayPoints)
-    
+    print(plan.wayPoints)
+    #plan.plot()
+
     with open("newnavigate.plan", "w") as outfile:
         outfile.write(json.dumps(plandict, indent=4))
 
